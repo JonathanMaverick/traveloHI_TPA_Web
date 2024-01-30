04f171c4cf13a032401260d482a88fdf968a1cd6
Jonathan Maverick <jonathanmaverick.jm@gmail.com>
2024-01-30 19:22:04 +0700

    last commit for initial

--- frontend/src/pages/home/travelohiHeader.tsx
+++ frontend/src/pages/home/travelohiHeader.tsx
@@ -4,7 +4,7 @@ export default function TravelohiHeader(){
     return(
         <>
         <h1>Why Travel with TraveloHI?</h1>
-        <p>Traveloka is a popular online travel agency known for its comprehensive services, including flight bookings, hotel reservations, holiday packages, activities, car rentals, and more. Offering a user-friendly platform and often providing promotions and discounts, Traveloka caters to travelers' diverse needs, allowing them to plan and book their entire trip conveniently. The platform's mobile app, customer reviews, and customer support further contribute to its appeal, making it a go-to choice for individuals seeking a one-stop solution for their travel arrangements.</p>
+        <p>TraveloHI is a popular online travel agency known for its comprehensive services, including flight bookings, hotel reservations, holiday packages, activities, car rentals, and more. Offering a user-friendly platform and often providing promotions and discounts, TraveloHI caters to travelers' diverse needs, allowing them to plan and book their entire trip conveniently. The platform's mobile app, customer reviews, and customer support further contribute to its appeal, making it a go-to choice for individuals seeking a one-stop solution for their travel arrangements.</p>
         </>
     )
 }
